f15f52d84270ab8c7cad8d2837deb1cc2d81b27c
Pako-12317 <patrykm10@nycstudents.net>
2024-09-20 12:10:35 +0000

    ,commit

--- src/main/java/com/example/project/TipCalculator.java
+++ src/main/java/com/example/project/TipCalculator.java
@@ -3,23 +3,22 @@ package com.example.project;
 import java.text.DecimalFormat;
 
 public class TipCalculator {
-    //WRITE YOUR PROGRAM IN calculateTip
-    public static String calculateTip(int people, int percent, double cost) { //You must use these  variable in your calculations
-        //DO NOT DELETE ANY OF THE CODE BELOW      
+    
+    public static String calculateTip(int people, int percent, double cost) { 
+             
         StringBuilder result = new StringBuilder();
-
+// I don't like decimal format.
         String pattern = "##.##";
         DecimalFormat decimalFormat = new DecimalFormat(pattern);
 
         double perPersonCostNoTip = (cost/people);
         double totalTip = (cost*((double)percent/100));
         double costWithTip = (cost+totalTip);
-        // double perPersonWithTip = Math.round((costWithTip/people)*100.0)/100.0;
         double perPersonWithTip = (costWithTip/people);
         double tipPerPerson = (totalTip/people);
         
         result.append("-------------------------------\n");
-        result.append("Total bill before tip: $"+decimalFormat.format(cost)+"\n"); //concatenate to this string. DO NOT ERASE AND REWRITE
+        result.append("Total bill before tip: $"+decimalFormat.format(cost)+"\n"); 
         result.append("Total percentage: "+decimalFormat.format(percent)+"%"+"\n");
         result.append("Total tip: $"+decimalFormat.format(totalTip)+"\n");
         result.append("Total Bill with tip: $"+decimalFormat.format(cost+totalTip)+"\n");
@@ -31,13 +30,13 @@ public class TipCalculator {
 
         return result.toString();
     }
-     //TEST YOUR PROGRAM IN main
+     //Key to not rounding is decimal format at the end when appending results.
      public static void main(String[] args) {
-        //try different values for people, percent, and cost to test your program before running test cases
+       
         int people = 12;
         int percent = 15;
         double cost = 566.97;             
         System.out.println(calculateTip(people,percent,cost));
     }
 }
-        
+   //All test cases work! Yay!     
